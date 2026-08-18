afc88236a26f3c6d0a329ca06ff21cf5bcda12ae
Your Name <tnsda@TNS_DA10>
2026-08-18 12:16:09 +0530

    commit

--- CG03_coreJAVA/src/com/tnsif/practiceprogram/Firstprogram.java
+++ CG03_coreJAVA/src/com/tnsif/practiceprogram/Firstprogram.java
@@ -2,6 +2,6 @@ package com.tnsif.practiceprogram;
 
 public class Firstprogram {
 	public static void main(String[] args) {
-		System.out.println("Welcome to java");
+		System.out.println("Welcome to JAVA");
 	}
 }
