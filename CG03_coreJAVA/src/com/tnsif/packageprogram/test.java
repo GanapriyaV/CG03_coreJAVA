40b5072fa6dff2df94d3ab1d01a33cf3e811fe58
Your Name <tnsda@TNS_DA10>
2026-08-24 13:00:37 +0530

    commit

--- CG03_coreJAVA/src/com/tnsif/packageprogram/test.java
+++ CG03_coreJAVA/src/com/tnsif/packageprogram/test.java
@@ -3,7 +3,10 @@ package com.tnsif.packageprogram;
 public class test {
 	public static void main(String[] args) {
 		Demoprogram d=new Demoprogram();
-		
+		System.out.println(d.a);
+		System.out.println(d.s);
+		System.out.println(d.c);
+		//System.out.println(d.f);
 		
 	}
 }
